6577f63885fa66f296edf77ca7fa8f8b8d37489e
joo.lee <joo10.lee@gmail.com>
2026-02-17 14:57:23 -0800

    Build: 0217-1457 - Update activities and sound logic

--- Projects/feelflow/sw.js
+++ Projects/feelflow/sw.js
@@ -1,11 +1,11 @@
-// sw.js - 아주 기본적인 서비스 워커
+// sw.js - Service Worker (Updated: 0217-1433)
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    // 앱이 작동하기 위한 필수 빈 처리
-    e.respondWith(
-      fetch(e.request).catch(() => caches.match(e.request))
-    );
-  });
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  // 앱이 작동하기 위한 필수 빈 처리
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
